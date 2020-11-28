4184eeb547d42c99dc86ebda6b28ee2371c4b00b
echigawa0921 <yuki.echigawa@di-v.co.jp>
2020-11-28 14:35:26 +0900

    contactページへのルーティング

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,10 @@ class PostsController < ApplicationController
     def index
       @posts = Post.all
     end
+
+    def contact
+      @posts = Post
+    end
     
     def new
         @post = Post.new
