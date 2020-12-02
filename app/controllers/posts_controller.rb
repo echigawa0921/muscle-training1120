9e5f11c8449c0f1bbb648c5b1a0f925e014e6ca4
echigawa0921 <yuki.echigawa@di-v.co.jp>
2020-12-02 21:18:20 +0900

    Active strageの導入

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,6 +30,6 @@ class PostsController < ApplicationController
   
     private
     def post_params
-      params.require(:post).permit(:name, :category_id, :message)
+      params.require(:post).permit(:name, :category_id, :message, :image)
     end
 end
