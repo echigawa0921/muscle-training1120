5d949ba4a439707fe3970ed625e5a624b0256245
echigawa0921 <yuki.echigawa@di-v.co.jp>
2021-01-27 12:01:00 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -6,7 +6,6 @@ worker_processes 1
 working_directory app_path
 
 pid "#{app_path}/tmp/pids/unicorn.pid"
-
 listen "#{app_path}/tmp/sockets/unicorn.sock"
 
 stderr_path "#{app_path}/log/unicorn.stderr.log"
